a31043d5bb73ecff117ec2db950b20bcd5123027
Rob Troke <RobT_Commit@icloud.com>
2025-07-02 15:41:10 -0400

    skip

--- terraform/environments/sand01/main.tf
+++ terraform/environments/sand01/main.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket" "demo_bucket" {
 }
 
 # --- Demo EC2 Instance (intentional error: no security group) ---
+# checkov:skip=CKV_AWS_126: Ensure that detailed monitoring is enabled for EC2 instances
 resource "aws_instance" "demo_ec2" {
   ami           = "ami-0c55b159cbfafe1f0" # Example AMI, update as needed
   instance_type = "t2.micro"
