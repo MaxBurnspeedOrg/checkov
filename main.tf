f972545e998f01627f52c51d934b74dfb8a82816
Rob Troke <RobT_Commit@icloud.com>
2025-07-03 11:54:13 -0400

    again

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#checkov:skip=CKV_AWS_20:The bucket is a public static content host
 resource "aws_s3_bucket" "foo-bucket" {
-  region        = var.region
-    #checkov:skip=CKV_AWS_20:The bucket is a public static content host
+  region        = var.region 
   bucket        = local.bucket_name
   force_destroy = true
   acl           = "public-read"
