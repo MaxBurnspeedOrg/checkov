9ada7f97a69f417c1470b079d5041951aa44d16b
Rob Troke <RobT_Commit@icloud.com>
2025-07-03 11:52:39 -0400

    skip

--- main.tf
+++ main.tf
@@ -2,14 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "insecure_bucket" {
-  bucket = "checkov-test-insecure-bucket"
-
-  # checkov:skip=CKV_AWS_52: "Public read access is acceptable for this test bucket"
-  acl = "public-read"  #  This would normally trigger CKV_AWS_52
-
-  tags = {
-    Name        = "Insecure bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "foo-bucket" {
+  region        = var.region
+    #checkov:skip=CKV_AWS_20:The bucket is a public static content host
+  bucket        = local.bucket_name
+  force_destroy = true
+  acl           = "public-read"
 }
